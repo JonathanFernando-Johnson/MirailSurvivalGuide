b9d4feaa56a67bcd731a3be085301aa7007f4702
GuillaumeDuc <ducoeurguillaume@gmail.com>
2018-12-06 23:43:18 +0100

    Added exemple login

--- mirail-survival-guide/src/App.js
+++ mirail-survival-guide/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Login from "./login";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Login />
         </header>
       </div>
     );
---
+++ mirail-survival-guide/src/login.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return <div className="App">Là tu mets ton truc</div>;
+  }
+}
+
+export default Login;
