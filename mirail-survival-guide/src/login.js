e61a967e8328902646ce5b1d522670ce0e710762
Nicolas Lejeune <nicolas.lejeune31@gmail.com>
2018-12-07 02:44:22 +0100

    Login

--- mirail-survival-guide/src/login.js
+++ mirail-survival-guide/src/login.js
@@ -1,9 +1,46 @@
 import React, { Component } from "react";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      redirectRegister: false
+    };
+  }
+
   render() {
-    return <div className="App">{this.props.value}</div>;
+    return <div className="Login">Bienvenue
+      <div>
+        <TextField
+          label="Utilisateur"
+          placeholder="Utilisateur"
+          onChange = {(event,newValue) => this.setState({username:newValue})}
+        />
+        <br />
+        <TextField
+           type="password"
+           label="Mot de passe"
+           placeholder="Mot de passe"
+           onChange = {(event,newValue) => this.setState({password:newValue})}
+         />
+         <br />
+         <Button variant="contained" color="primary" className="connect">
+            Connexion
+         </Button>
+         <br />
+         Pas de compte ?
+         <br />
+         <Button variant="contained" color="primary" className="register">
+            Inscription
+         </Button>
+      </div>
+    </div>;
   }
 }
+
 
 export default Login;
